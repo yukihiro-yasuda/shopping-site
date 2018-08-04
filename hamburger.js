912b77712f5832a73aa126a9c17192bc2c9f3290
EC2 Default User <ec2-user@ip-172-31-47-243.us-east-2.compute.internal>
2018-08-04 12:21:25 +0000

    messge

--- hamburger.js
+++ hamburger.js
@@ -1,12 +1,18 @@
 
 $(document).ready(function(){
-  $(".page_link").click(function(){
+  $("#page_link").click(function(){
     $(".page_menu_toggle").slideToggle();
   });
 });
 
 $(document).ready(function(){
-  $(".categories_link").click(function(){
+  $("#categories_link").click(function(){
     $(".link_menu_toggle").slideToggle();
+  });
+});
+
+$(document).ready(function(){
+  $(".sp_navi").click(function(){
+    $(".sp_navi_li").slideToggle();
   });
 });
